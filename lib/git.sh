5d021097e7f82c3b9cda91461caa0c5625774ca0
Ryan Armstrong <ryan@cavaliercoder.com>
2017-04-26 13:25:58 +0800

    Updated git-tree output formatting

--- lib/git.sh
+++ lib/git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-alias git-tree="git log --graph --oneline --all --decorate"
+alias git-tree="git log --graph --date=relative  --decorate --pretty=\"format:%C(yellow)%h %C(blue)%>(12)%ad %C(green)%<(7)%aN%C(auto)%d %C(reset)%s\""
